a568aeb61896e43ddba63c41d3766d10f7731c43
hide <hide85x@gmail.com>
2020-04-13 16:23:22 +0200

    trying to fix right marign problem, resizing modal window

--- docs/Klima-Roll.e31bb0bc.js
+++ docs/Klima-Roll.e31bb0bc.js
@@ -3328,7 +3328,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56344" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56374" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
